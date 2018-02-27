29713df0ca6ff1a296866a8c437f105e4322e8bb
Eric Ostler <eostler13@gmail.com>
2018-02-26 21:12:59 -0700

    added character objects

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+//Execute this code when the DOM has fully loaded.
+$(document).ready(function(){
+    var characters = {
+        "Natsu": {
+            name: "Natsu Dragneel",
+            health: 180,
+            attack: 12,
+            imageUrl: "assets/images/Natsu.jpg",
+            enemyAttackBack: 12
+        },
+        "Gajeel" : {
+            name: "Gajeel Redfox",
+            health: 140,
+            attack: 10,
+            imageUrl: "assets/images/Gajeel.jpg",
+            enemyAttackBack: 12
+        },
+        "Wendy" : {
+            name: "Wendy Marvell",
+            health: 100,
+            attack: 14,
+            imageUrl: "assets/images/Wendy.jpg",
+            enemyAttackBack: 6
+        },
+        "Laxus" : {
+            name: "Laxus Dreyar",
+            health: 150,
+            attack: 8,
+            imageUrl: "assets/images/Laxus.png",
+            enemyAttackBack: 16
+        }
+    };
+});
